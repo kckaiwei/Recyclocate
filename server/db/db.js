9b707f1dc1bf53aeb3553257bcc8e3b59ca611fb
Kathleen McDaniel <kathleen_mc.daniel@yahoo.com>
2019-11-17 11:39:05 -0500

    fix: working on setting up webpack and config environment

--- server/db/db.js
+++ server/db/db.js
@@ -1,21 +1,26 @@
-const Sequelize = require('sequelize');
-const pkg = require('../../package.json');
-require('../../secrets');
-require('dotenv').config();
+const Sequelize = require('sequelize')
+const pkg = require('../../package.json')
+require('../../secrets')
+require('dotenv').config()
 
-const databaseName =
-  pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '');
+const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  process.env.REACT_APP_DATABASE_URL ||
+    `postgres://localhost:5432/${databaseName}`,
   {
-    logging: false
+    logging: false,
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: true
+    }
   }
-);
-module.exports = db;
+)
+module.exports = db
 
 // This is a global Mocha hook used for resource cleanup.
 // Otherwise, Mocha v4+ does not exit after tests.
 if (process.env.NODE_ENV === 'test') {
-  after('close database connection', () => db.close());
+  after('close database connection', () => db.close())
 }
